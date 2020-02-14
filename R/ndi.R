3dce19b2bab260e7c6137f220388d38f6ca1ea56
Ivan C <ivedcastr@gmail.com>
2020-02-14 17:12:13 -0500

    added deleted var, re-styled the code

--- R/ndi.R
+++ R/ndi.R
@@ -1,42 +1,70 @@
 
-ndi <- function(arg1,arg2){
+ndi <- function(state,county){
   
-  vars <- c("B17001_002", "B17001_001", "B06009_002" , "B06009_001","B09008_011", 
-            "B09008_001","B08124_002", "B08124_001", "B25014_005",
-            "B25014_006",  "B25014_007","B25014_011", "B25014_012", "B25014_013", 
-            "B25014_001", "B19058_002", "B19058_001", "C23002A_008E", "C23002A_021E", 
-            "C23002A_006E", "C23002A_019E", "B19001_003", "B19001_004",
+  vars <- c("B17001_002", "B17001_001", "B06009_002" , 
+            "B06009_001", "B09008_011", "B09008_001",
+            "B08124_002", "B08124_001", "B25014_005",
+            "B25014_006", "B25014_007", "B25014_011", 
+            "B25014_012", "B25014_013", "B25014_001", 
+            "B19058_002", "B19058_001", "C23002A_008E", 
+            "C23002A_021E", "C23002A_006E", "C23002A_019E", 
+            "B19001_002","B19001_003", "B19001_004",
             "B19001_005", "B19001_006", "B19001_001")
   
-  if (missing(arg2)) {
-    arg2 <- NULL
+  if (missing(county)) {
+    county <- NULL
   } else {
-    arg2 <- arg2
+    county <- county
   }
+  
+  acs_data <- tidycensus::get_acs(geography = "tract", 
+                                  variables = vars,
+                                  state = state,  
+                                  county = county,
+                                  output = "wide") %>%
+    dplyr::mutate(pct_poverty = B17001_002E / B17001_001E,
+           pct_noHS = B06009_002E / B06009_001E,
+           pct_FHH = B09008_011E / B09008_001E,
+           pct_mgmt = B08124_002E /  B08124_001E, 
+           pct_crowd =  (B25014_005E + B25014_006E + B25014_007E + 
+                           B25014_011E + B25014_012E + B25014_013E) / B25014_001E,
+           pct_pubassist = B19058_002E / B19058_001E,
+           pct_unempl = (C23002A_008E + C23002A_021E) / (C23002A_006E + C23002A_019E),
+           pct_under30K =( B19001_002E + B19001_003E + B19001_004E +
+                             B19001_005E +  B19001_006E) / B19001_001E)
 
-  acs_data <- get_acs(geography = "tract", variables =vars,state = arg1,  
-                    county = arg2,output = "wide") %>%
-    mutate(pct_poverty = B17001_002E/B17001_001E,
-          pct_noHS = B06009_002E / B06009_001E,
-          pct_FHH = B09008_011E / B09008_001E,
-          pct_mgmt = B08124_002E /  B08124_001E, 
-          pct_crowd =  (B25014_005E +B25014_006E+ B25014_007E + 
-                         B25014_011E + B25014_012E + B25014_013E) / B25014_001E,
-          pct_pubassist = B19058_002E/B19058_001E,
-          pct_unempl = (C23002A_008E + C23002A_021E) / (C23002A_006E + C23002A_019E),
-          pct_under30K =( B19001_002E+B19001_003E+B19001_004E+B19001_005E +
-                           B19001_006E) / B19001_001E)
-
-  values  <-  acs_data %>% select(pct_poverty,pct_noHS,pct_FHH,pct_mgmt,pct_crowd,
-                                pct_pubassist, pct_unempl,pct_under30K) %>% as.matrix()
+  values  <-  acs_data %>% 
+    dplyr::select(pct_poverty,
+                  pct_noHS,
+                  pct_FHH,
+                  pct_mgmt,
+                  pct_crowd,                              
+                  pct_pubassist, 
+                  pct_unempl,
+                  pct_under30K) %>% 
+    as.matrix()
 
   values[is.nan(values)] <- 0
-  ND <- principal(values,nfactors = 1)          
-  NDI <- cbind(acs_data,ND$scores) 
-  NDI <- NDI %>% select(NAME,GEOID,PC1,57:64) %>% 
-    separate(NAME, into = c("Tract", "County","State"), sep = ",") %>% 
-    mutate(County = str_trim(County), State = str_trim(State)) %>%
-    rename(NDI = PC1)  %>% as_tibble() 
+  
+  ND <- psych::principal(values,
+                         nfactors = 1)          
+  
+  NDI <- cbind(acs_data,
+               ND$scores) 
+  
+  NDI <- NDI %>% 
+    dplyr::select(NAME, 
+                  GEOID, 
+                  PC1, 
+                  57:64) %>% 
+    tidyr::separate(NAME, 
+                    into = c("Tract", "County", "State"), 
+                    sep = ",") %>% 
+    dplyr::mutate(County = stringr::str_trim(County), 
+                  State = stringr::str_trim(State)) %>%
+    rename(NDI = PC1)  %>% 
+    dplyr::as_tibble() 
   
   return(NDI)
+  
 }
